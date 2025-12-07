1800b004001677295ec87d11fd0ba18dee3f9ace
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-07 21:12:07 +0500

    Task2

---
+++ Tyuiu.RadkoVO.Sprint2.Task2.V1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.RadkoVO.Sprint2.Task2.V1.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Радько В. О.  | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Радько Варвара Олеговна  | СМАРТБ-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры и*");
+        Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение х = ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение у = ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        DataService ds = new DataService();
+        bool res = ds.CheckDotInShadedArea(x, y);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        if (res)
+        {
+            Console.WriteLine("Точка в области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не в области");
+        }
+        Console.ReadKey();
+    }
+}
