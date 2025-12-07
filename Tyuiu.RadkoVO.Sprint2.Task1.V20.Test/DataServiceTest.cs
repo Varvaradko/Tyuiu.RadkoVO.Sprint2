df1c57388783fb738ec9697097ec61b0f3e815a8
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-07 20:54:04 +0500

    Task1

---
+++ Tyuiu.RadkoVO.Sprint2.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RadkoVO.Sprint2.Task1.V20.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint2.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetLogicOperations()
+        {
+            DataService ds = new DataService();
+
+            int a = 242;
+            int b = 155;
+            int c = 456;
+            int d = 17;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { false, false, false, true, true, true };
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.RadkoVO.Sprint2.Task1.V20/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.RadkoVO.Sprint2.Task1.V20.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint2.Task1.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 242;
+            int b = 155;
+            int c = 456;
+            int d = 17;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.Title = "Спринт #2 | Выполнил: Радько В. О. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Радько Варвара Олеговна | СМАРТб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений и логических операций, а также *");
+            Console.WriteLine("* арифметических выражений, которая вернет логическую последовательность: *");
+            Console.WriteLine("* (False, False, False, True, True, True),                                *");
+            Console.WriteLine("* при a = 242, b = 155, c = 456, d = 17                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            foreach (var i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
