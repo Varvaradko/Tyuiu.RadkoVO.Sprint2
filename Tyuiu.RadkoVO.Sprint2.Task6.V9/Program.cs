5cf8d8c125c2709d566d83439b9a7757846f7247
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-10 06:46:22 +0500

    Task6

---
+++ Tyuiu.RadkoVO.Sprint2.Task6.V9/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.RadkoVO.Sprint2.Task6.V9.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint2.Task6.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Радько В. О. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Радько Варвара Олеговна | СМАРТб-25-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int Month, Day;
+            Console.WriteLine("Введите номер месяца: ");
+            Month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Введите номер дня: ");
+            Day = Convert.ToInt32(Console.ReadLine());
+
+            if ((Month > 12 || Day > 31) || (Month % 2 != 0 && Day == 31) || (Month == 2 && Day > 28))
+            {
+                Console.WriteLine("Такой даты не сушествует.");
+            }
+            else
+            {
+                Console.WriteLine(ds.FindDateOfNextDay(Month, Day));
+            }
+        }
+    }
+}
