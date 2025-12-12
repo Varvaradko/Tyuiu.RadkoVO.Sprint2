71bfcfb73290128e5199c2c420321cacb9443127
Варвара Радько <Tyuiu.RadkoVO@outlook.com>
2025-12-13 01:15:29 +0500

    Task7

---
+++ Tyuiu.RadkoVO.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadkoVO.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.RadkoVO.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService dataService = new DataService();
+
+            Assert.IsTrue(dataService.CheckDotInShadedArea(1, 1));
+            Assert.IsTrue(dataService.CheckDotInShadedArea(0, 1.5));
+
+            Assert.IsFalse(dataService.CheckDotInShadedArea(0, -1));
+            Assert.IsFalse(dataService.CheckDotInShadedArea(2.5, 0));
+            Assert.IsFalse(dataService.CheckDotInShadedArea(0.5, 0.5));
+        }
+    }
+}
